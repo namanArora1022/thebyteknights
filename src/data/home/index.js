f87e7f236af75478bb6a94658637c988e63b68d1
Naman Arora <71423632+namanArora1022@users.noreply.github.com>
2021-09-09 14:17:12 +0530

    what we do section done

---
+++ src/data/home/index.js
@@ -0,0 +1,38 @@
+import Coding from '../../assets/home/features/work.svg';
+import Video from '../../assets/home/features/video.svg';
+import Photo from '../../assets/home/features/photo.svg';
+import Quiz from '../../assets/home/features/quiz.svg';
+import Design from '../../assets/home/features/design.svg';
+
+export const features = [
+    {
+        id: 1,
+        name: 'Coding',
+        img: Coding,
+        description: 'We have a keen intrest in coding , software development , websites and apps in general'
+    },
+    {
+        id: 2,
+        name: 'Video Editing',
+        img: Video,
+        description: `Video Editing is a demand of today's modern world ,we can alter and devlop amazing videos use video editing softwares`
+    },
+    {
+        id: 3,
+        name: 'Photography',
+        img: Photo,
+        description: `It is the story we failed to put into words.In Photography there is a reality so subtle that it becomes more real than reality.`
+    },
+    {
+        id: 4,
+        name: 'Quizzes',
+        img: Quiz,
+        description: 'We conduct various quizzes based on knowledge of computer applications, which develops a keen interest among students'
+    },
+    {
+        id: 5,
+        name: 'Graphic Designing',
+        img: Design,
+        description: 'Graphic Designing helps students to use their creative minds to devlop the various user interfaces and user experiences(UI/UX)'
+    },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 
+import { features } from '../data/home';
+
 const Home = () => {
     return (
         <div>
             {/* Landing */}
             <main className='h-screen bg-home-landing bg-no-repeat bg-cover sm:bg-center text-white flex justify-center items-center flex-col pt-32'>
-                {/* <div className='h-screen bg-gray-900 opacity-70 '>
-                
-                </div> */}
                 <h1 className="font-Rancho text-8xl text-center md:text-9xl">The Byte Knights</h1>
                 <h2 className="font-Rancho text-5xl text-center md:text-7xl mb-10">To Live to Learn</h2>
             </main>
+            {/* What we do */}
+            <section className='flex justify-center items-center flex-col py-20 md:px-36 px-20'>
+                <h2 className='text-4xl Pink-border mb-8'>What we do</h2>
+                <div className='flex flex-wrap justify-center items-center'>
+                    {features.map(feature => (
+                        <div key={feature.id} className='flex flex-col justify-center items-center my-6 mx-10'>
+                            <img 
+                            className='object-cover h-16 p-2 rounded-lg bg-Pink'
+                            src={feature.img} 
+                            alt={feature.name} 
+                            />
+                            <h3 className='my-3 font-semibold'>{feature.name}</h3>
+                            <p className='w-52 text-center'>{feature.description}</p>
+                        </div>
+                    ))}
+                </div>
+            </section>
         </div>
     )
 }
