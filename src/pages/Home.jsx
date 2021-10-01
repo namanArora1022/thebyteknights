e1010d5c484f25c52cf3e9e31d57fe10fcd791ca
TheByteKnights <byteknights@gmail.com>
2021-10-01 18:28:16 +0530

    aniamtions added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,18 +8,23 @@ import Feature from '../components/Home/Feature';
 import Testimonial from '../components/Home/Testimonial';
 import Particles from '../components/Home/Particles';
 
+// Animations
+import { Bounce } from 'react-awesome-reveal';
+
 const Home = () => {
     return (
         <div>
             {/* Landing */}
             <main className='h-screen bg-home-landing bg-no-repeat bg-cover sm:bg-center text-white flex justify-center items-center flex-col pt-32'>
-                <Particles/>
+                <Particles />
                 <h1 className="font-Rancho text-8xl text-center md:text-9xl">The Byte Knights</h1>
                 <h2 className="font-Rancho text-5xl text-center md:text-7xl mb-10">To Live to Learn</h2>
             </main>
             {/* What we do */}
             <section className='flex justify-center items-center flex-col py-20 bg-gray-100'>
-                <h2 className='text-4xl Pink-border mb-8'>&lt;What we do/&gt;</h2>
+                <Bounce direction='down' triggerOnce>
+                    <h2 className='text-4xl Pink-border mb-8'>&lt;What we do/&gt;</h2>
+                </Bounce>
                 <div className='flex flex-wrap justify-center items-center'>
                     {features.map(feature => (
                         <Feature feature={feature} key={feature.id} />
@@ -28,7 +33,9 @@ const Home = () => {
             </section>
             {/* Testimonials */}
             <section className='flex justify-center items-center flex-col py-20'>
-                <h2 className='text-4xl Pink-border mb-8'>&lt;Testimonials/&gt;</h2>
+                <Bounce direction='down' triggerOnce>
+                    <h2 className='text-4xl Pink-border mb-8'>&lt;Testimonials/&gt;</h2>
+                </Bounce>
                 <div className='flex flex-wrap justify-center items-center'>
                     {testimonials.map(testimonial => (
                         <Testimonial testimonial={testimonial} key={testimonial.id} />
