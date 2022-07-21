8dc373d7d4dca9ee7549c95751713586c2a05da2
Naman Arora <namanarora1022@gmail.com>
2022-07-21 17:37:47 +0530

    added sanity

---
+++ src/lib/client.js
@@ -0,0 +1,11 @@
+import sanityClient from '@sanity/client';
+
+const client = sanityClient({
+    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+    dataset: 'production',
+    apiVersion: '2022-07-19',
+    token: process.env.REACT_APP_SANITY_TOKEN,
+    useCdn: true
+});
+
+export default client;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import client from '../lib/client';
 
 // Data
 import { features, testimonials } from '../data/home';
@@ -12,38 +13,61 @@ import Particles from '../components/Home/Particles';
 import { Bounce } from 'react-awesome-reveal';
 
 const Home = () => {
+    const [lines, setLines] = useState({ firstLine: '', secondLine: '' });
+
+    useEffect(() => {
+        const query = '*[_type == "home"]';
+        client.fetch(query).then(data => {
+            setLines({
+                firstLine: data[0].firstLine,
+                secondLine: data[0].secondLine
+            });
+        });
+    }, []);
+
     return (
         <div>
             {/* Landing */}
-            <main className='h-screen bg-home-landing bg-no-repeat bg-cover sm:bg-center text-white flex justify-center items-center flex-col pt-32'>
+            <main className="h-screen bg-home-landing bg-no-repeat bg-cover sm:bg-center text-white flex justify-center items-center flex-col pt-32">
                 <Particles />
-                <h1 className="font-Rancho text-8xl text-center md:text-9xl">The Byte Knights</h1>
-                <h2 className="font-Rancho text-5xl text-center md:text-7xl mb-10">To Live to Learn</h2>
+                <h1 className="font-Rancho text-8xl text-center md:text-9xl">
+                    {lines.firstLine}
+                </h1>
+                <h2 className="font-Rancho text-5xl text-center md:text-7xl mb-10">
+                    {lines.secondLine}
+                </h2>
             </main>
             {/* What we do */}
-            <section className='flex justify-center items-center flex-col py-20 bg-gray-100'>
-                <Bounce direction='down' triggerOnce>
-                    <h2 className='text-4xl Pink-border mb-8'>&lt;What we do/&gt;</h2>
+            <section className="flex justify-center items-center flex-col py-20 bg-gray-100">
+                <Bounce direction="down" triggerOnce>
+                    <h2 className="text-4xl Pink-border mb-8">
+                        &lt;What we do/&gt;
+                    </h2>
                 </Bounce>
-                <div className='flex flex-wrap justify-center items-center'>
+                <div className="flex flex-wrap justify-center items-center">
                     {features.map(feature => (
                         <Feature feature={feature} key={feature.id} />
                     ))}
                 </div>
             </section>
             {/* Testimonials */}
-            <section className='flex justify-center items-center flex-col py-20'>
-                <Bounce direction='down' triggerOnce>
-                    <h2 className='text-4xl Pink-border mb-8'>&lt;Testimonials/&gt;</h2>
+            <section className="flex justify-center items-center flex-col py-20">
+                <Bounce direction="down" triggerOnce>
+                    <h2 className="text-4xl Pink-border mb-8">
+                        &lt;Testimonials/&gt;
+                    </h2>
                 </Bounce>
-                <div className='flex flex-wrap justify-center items-center'>
+                <div className="flex flex-wrap justify-center items-center">
                     {testimonials.map(testimonial => (
-                        <Testimonial testimonial={testimonial} key={testimonial.id} />
+                        <Testimonial
+                            testimonial={testimonial}
+                            key={testimonial.id}
+                        />
                     ))}
                 </div>
             </section>
         </div>
-    )
-}
+    );
+};
 
 export default Home;
